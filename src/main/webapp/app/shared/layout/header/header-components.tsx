3ec3ea1a3e3b31b649cee68a0114550e0bc83999
Edwin Njeru <mailnjeru@gmail.com>
2021-01-20 14:50:43 +0300

    Add app designations to homepage

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -15,7 +15,7 @@ export const BrandIcon = props => (
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
-    <span className="brand-title">PrepaymentsDev</span>
+    <span className="brand-title">Prepayments</span>
     <span className="navbar-version">{appConfig.VERSION}</span>
   </NavbarBrand>
 );
